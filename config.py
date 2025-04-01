a58d079afed49d5f1446d73368dde12fd996bb12
Abhilash Paul <paul.abhilash@hotmail.com>
2025-04-01 09:17:39 -0600

    increase number of epochs to 30

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import torch
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-BATCH_SIZE = 16
-NUM_EPOCHS = 20
+BATCH_SIZE = 20
+NUM_EPOCHS = 30
 
 saved_model_path = "output/best_model.pth"
 data_dir = "xray_dataset"
